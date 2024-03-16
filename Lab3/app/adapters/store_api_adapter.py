4bc7d056642fc7977c37f0329487e24118eb637e
Sia <assiya.serb@gmail.com>
2024-03-16 22:16:30 +0200

    Lab3

---
+++ Lab3/app/adapters/store_api_adapter.py
@@ -0,0 +1,19 @@
+import json
+import logging
+from typing import List
+
+import pydantic_core
+import requests
+
+from app.entities.processed_agent_data import ProcessedAgentData
+from app.interfaces.store_gateway import StoreGateway
+
+
+class StoreApiAdapter(StoreGateway):
+    def __init__(self, api_base_url):
+        self.api_base_url = api_base_url
+
+    def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]):
+        json_data = [json.loads(data.json()) for data in processed_agent_data_batch]
+        response: requests.Response = requests.post(self.api_base_url + "/processed_agent_data/", json=json_data)
+        return response.status_code == 200
